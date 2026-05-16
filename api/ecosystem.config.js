7a017798959e7e7acab726bc2f274dec28845559
Marcos Macedo <marcosmacedo@MacBook-Air-de-Marcos.local>
2026-05-16 12:23:26 -0300

    chore(api): enable pm2 sticky sessions for socket stability

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: './app.js',
       instances: 2,
       exec_mode: 'cluster',
+      sticky: true,
       max_memory_restart: '300M',
       env_production: {
         NODE_ENV: 'development',
